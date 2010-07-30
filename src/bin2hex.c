da4a7ddc358e512a3b8614ca7eb2fcbf521d49e8
Toni Spets <toni.spets@iki.fi>
2010-07-30 23:20:09 +0300

    Fix bin2hex and plugman depencies, add 2010 to license, rewrite bin2hex again

--- src/bin2hex.c
+++ src/bin2hex.c
@@ -1,44 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-#define BUF_SIZE 10240
 
 int main(int argc, char **argv)
 {
-	unsigned char *buf;
-	ssize_t size, i;
+	unsigned char byte;
 
-	buf = malloc(BUF_SIZE);
-	if(!buf) {
-		perror("Error allocating memory");
-		return 1;
+	printf("unsigned char %s[] = { ", argc > 1 ? argv[1] : "bin2hex");
+
+	while(read(0, &byte, sizeof(byte)) > 0) {
+		printf("0x%02X, ", byte);
 	}
 
-	size = read(0, buf, BUF_SIZE);
-	if(size < 0) {
-		perror("Error reading from stdin");
-		return 1;
-	}
-
-	if(size > 0) {
-		printf("unsigned char %s[%d] = {\n\t", argc > 1 ? argv[1] : "bin2hex", size);
-
-		for(i=0; i<size; i++) {
-
-			printf("0x%02X", *(buf+i));
-
-			if((i+1) < size) {
-				printf(", ");
-				if((i+1) % 8 == 0) {
-					printf("\n\t");
-				}
-			}
-		}
-
-		printf("\n};\n");
-	}
-
-	free(buf);
+	printf("};\n");
 
 	return 0;
 }
