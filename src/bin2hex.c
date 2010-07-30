4a2d2d295c49264ffca48403d9b7e11d56ae3131
Toni Spets <toni.spets@iki.fi>
2010-07-30 21:19:37 +0300

    No need to depend on luac

--- src/bin2hex.c
+++ src/bin2hex.c
@@ -1,43 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BUF_SIZE 10240
+
 int main(int argc, char **argv)
 {
-    FILE *fh;
-    unsigned char byte;
-    long i;
-    long size;
+	unsigned char *buf;
+	ssize_t size, i;
 
-    if(argc < 2) {
-        fprintf(stderr, "Usage: %s <binary> [array name]\n", argv[0]);
-        exit(1);
-    }
+	buf = malloc(BUF_SIZE);
+	if(!buf) {
+		perror("Error allocating memory");
+		return 1;
+	}
 
-    fh = fopen(argv[1], "r");
-    if(!fh) {
-        fprintf(stderr, "%s: ", argv[1]);
-        perror(NULL);
-        exit(1);
-    }
+	size = read(0, buf, BUF_SIZE);
+	if(size < 0) {
+		perror("Error reading from stdin");
+		return 1;
+	}
 
-    fseek(fh, 0, SEEK_END);
-    size = ftell(fh);
-    fseek(fh, 0, SEEK_SET);
+	if(size > 0) {
+		printf("unsigned char %s[%d] = {\n\t", argc > 1 ? argv[1] : "bin2hex", size);
 
-    printf("unsigned char %s[%ld] = {\n\t", argc > 2 ? argv[2] : "bin2hex", size);
+		for(i=0; i<size; i++) {
 
-    for(i = 0; i < size; i++) {
-        byte = (unsigned char)fgetc(fh);
-        printf("0x%02X", byte);
+			printf("0x%02X", *(buf+i));
 
-        if((i+1) < size) {
-            printf(", ");
-            if((i+1) % 8 == 0)
-                printf("\n\t");
-        }
-    }
+			if((i+1) < size) {
+				printf(", ");
+				if((i+1) % 8 == 0) {
+					printf("\n\t");
+				}
+			}
+		}
 
-    printf("\n};\n");
+		printf("\n};\n");
+	}
 
-    return 0;
+	free(buf);
+
+	return 0;
 }
